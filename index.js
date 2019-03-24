8de36935d57527be7d556988b258a33681b6e763
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-24 14:29:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine (updateLine) {
-  for (var i = 0; i < katzDeliLine.length; i++) {
-    return "The line is currently: " + katzDeliLine.length[i] + ". " + katzDeliLine[i]
-  } else {
-    return "The line is currently empty."
+function currentLine(line){
+  if(!line.length) {
+    return "The line is currently empty.";
   }
+  var lineNamesandNumbers = [];
+
+  for(var i=0; i<line.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + lineNamesandNumbers)
+  return "The line is currently: " + lineNamesandNumbers.join(', ');
+}
