55afcc14c6dba6e43c4023efe0b6ccee4bff3054
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-24 13:10:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine) {
     for (var i = 0; i > katzDeliLine.length; i++) {
       line.push(`${i + 1}.${katzDeliLine[i]}`)
     }
-    return `The line is currently:` + katzDeliLine
+    return `The line is currently:` + katzDeliLine.length + "." + line
   } else {
     return "The line is currently empty."
   }
